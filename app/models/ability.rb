ea7bfdf0f8bacae8b44eb02cda85619c2fe64c2d
westbee <mr.westbee@gmail.com>
2016-08-24 00:11:07 +0200

    5.10 Admin role and moderating user comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,9 +6,9 @@ class Ability
     #
        user ||= User.new # guest user (not logged in)
     if user.admin?
-       can :manage, User, id: user.id
+       can :manage, :all
     else
-       can :read, Order, user_id: user.id
+       can :read, :all
     end
     #
     # The first argument to `can` is the action you are giving the user
