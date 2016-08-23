1f239ad3295268e198bec48f850748705e38bf27
westbee <mr.westbee@gmail.com>
2016-08-23 21:27:40 +0200

    5.9 validation and pagination

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,9 @@ class Comment < ApplicationRecord
 
 	scope :rating_desc, -> { order(rating: :desc) }
 
+	validates :body, presence: true
+ 	validates :user, presence: true
+  	validates :product, presence: true
+  	validates :rating, numericality: { only_integer: true }
+
 end
