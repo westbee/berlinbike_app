ae373e54104d3b56ce2a886cf47432a068bb6909
westbee <mr.westbee@gmail.com>
2016-08-31 19:43:14 +0200

    6.2 Unit Tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe Product  do
+
+	context "when the product has comments" do
+	
+
+		before do
+			@product = Product.create!(name: "race bike")
+			@user = User.create!(first_name: "Adam", last_name: "Tester", email: "test@example.com" , password: "abcdefgh")
+			@product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+			@product.comments.create!(rating: 3, user: @user, body: "it's fine")
+			@product.comments.create!(rating: 5, user: @user, body: "great bike!")
+		end
+
+		it "returns the average rating of all comments" do
+			expect(@product.average_rating).to eq 3
+		end
+
+		it "is not valid" do
+			expect(Product.new(description: "Nice nike")).not_to be_valid
+		end
+
+	end
+end
