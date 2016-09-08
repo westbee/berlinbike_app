ec78671a3228ff8b99ab738b55c66a7ab5582a57
westbee <mr.westbee@gmail.com>
2016-09-08 19:09:18 +0200

    trying to fix price issue on heroku

--- db/migrate/20160905194843_change_data_type_of_products_price_to_decimal.rb
+++ db/migrate/20160905194843_change_data_type_of_products_price_to_decimal.rb
@@ -1,11 +1,7 @@
 class ChangeDataTypeOfProductsPriceToDecimal < ActiveRecord::Migration[5.0]
 	
-	def self.up
+	def change
 		change_column :products, :price, :decimal, precision: 8, scale: 2
-	end
-
-	def self.down
-		change_column :products, :price, :string
 	end
 
 end
