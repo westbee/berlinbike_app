c03f9a14b4cffa91f81b9c7c6be042cef9ad4641
westbee <mr.westbee@gmail.com>
2016-09-12 19:39:30 +0200

    6.14 Active Job

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,13 @@ class CommentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
+    @user = current_user
 
     respond_to do |format|
       if @comment.save
         # ActionCable.server.broadcast 'product_channel', comment: @comment
         # ProductChannel.broadcast_to @product.id, comment: @comment
-        ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user })
+        # ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user })
         
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
